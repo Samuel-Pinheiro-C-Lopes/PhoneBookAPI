401e72f0a00a0494572a7a60a1a36246f06e4d9d
Samuel-Pinheiro-C-Lopes <samuelpinheiroclopes@gmail.com>
2025-11-13 15:04:13 -0300

    Added authentication repositories

---
+++ src/main/java/io/github/samuel_pinheiro_c_lopes/phone_book/authentication/repositories/RoleRepository.java
@@ -0,0 +1,11 @@
+package io.github.samuel_pinheiro_c_lopes.phone_book.authentication.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import io.github.samuel_pinheiro_c_lopes.phone_book.authentication.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
+}
